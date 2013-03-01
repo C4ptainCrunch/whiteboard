1c00210814dfb9fa1b9af7a9fc99580edfc62dc7
Titouan Christophe <titouanchristophe@gmail.com>
2013-03-01 19:32:21 +0100

    [add] Graph manipulation methods + unit tests

--- graph/models.py
+++ graph/models.py
@@ -6,7 +6,61 @@ from keywords.models import Keyword
 class Node(PolymorphicModel):
     """Base class for all P402 objects"""
     name = models.CharField(max_length=160)
-    children = models.ManyToManyField("self", symmetrical=False)
+    _children = models.ManyToManyField("self", symmetrical=False)
+    
+    def classBasename(self):
+        """Return the class name without modules prefix"""
+        klass = str(type(self)) # "<class 'foo'>"
+        j = len(klass)-2
+        i = j
+        while i>0 and klass[i-1]!='.':
+            i -= 1
+        return klass[i:j]
+    
+    
+    def __repr__(self):
+        return '<%s:%d "%s">'%(self.classBasename(), self.pk, self.name)
+    
+    
+    def children(self):
+        """Return a list of all self's children"""
+        return self._children.all()
+    
+    
+    def ancestors(self):
+        """Return a list of all self's ancestors"""
+        return Node.objects.filter(_children=self)
+    
+    
+    def hasCycle(self, traversed):
+        """Recursively walk the graph to find any loop"""
+        res = False
+        if self in traversed:
+            res = True
+        else:
+            traversed.append(self)
+            for child in self.children():
+                if child.hasCycle(traversed):
+                    res = True
+                    break
+            traversed.pop()
+        return res
+    
+    
+    def attach(self, child, acyclic_check=True):
+        """
+        Attach a new child to self and return True. If acyclic_check evaluates
+        to True, and a loop occurs with this new edge, don't add the new child
+        and return False.
+        """
+        res = True
+        if acyclic_check and child.hasCycle([self]):
+            res = False
+        else:
+            self._children.add(child)
+            self.save()
+        return res
+    
 
 
 class Category(Node):
--- graph/tests.py
+++ graph/tests.py
@@ -6,11 +6,39 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from graph.models import Node, Course, Category
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_graph(self):
+        """Basic graph manipulation"""
+        a, b, c = (Node.objects.create() for i in range(3))
+        a.attach(b); b.attach(c) #A->B->C
+        
+        self.assertTrue(a.attach(c), 'A->B->C && A->C')
+        
+        self.assertIn(b, a.children(), 'Graph walk')
+        self.assertIn(a, b.ancestors(), 'Reverse graph walk')
+        self.assertFalse(c.attach(a), 'Cycle')
+        self.assertTrue(c.attach(a, False), 'Force cyclic attach')
+        self.assertFalse(a.attach(b), 'Attach existing child')
+    
+    
+    def test_polymorph(self):
+        ulb = Category.objects.create(name="ULB", description="Universite Libre de Bruxelles")
+        
+        polytek = Category.objects.create(name="Polytech", description="Ecole Polytechnique")
+        ulb.attach(polytek)
+        ba1p = Category.objects.create(name="BA1-POLY", description="Bachelier 1 en polytech")
+        polytek.attach(ba1p)
+        
+        science = Category.objects.create(name="FS", description="Faculte des Sciences")
+        ulb.attach(science)
+        ba1i = Category.objects.create(name="BA1-INFO", description="Bachelier 1 en sciences informatiques")
+        science.attach(ba1i)
+        
+        prog = Course.objects.create(name="Programmation 1", mnemonic="INFO-F-101", teacher="tmassart@ulb.ac.be")
+        ba1p.attach(prog)
+        ba1i.attach(prog)
+        
+        self.assertIn(ba1p, prog.ancestors(), 'INFO-F-101 for polytech')
+        self.assertIn(ba1i, prog.ancestors(), 'INFO-F-101 for comp. sc.')
