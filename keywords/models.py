2e9712c62c83c569725deab440e8b00366fb86bd
C4ptainCrunch <nikita.marchant@gmail.com>
2013-03-02 17:40:19 +0100

    [fix] fixes tz errors on insert

--- keywords/models.py
+++ keywords/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Keyword(models.Model):
     """
-    Keywords are a comfortable way to group object that are far from each other 
+    Keywords are a comfortable way to group object that are far from each other
     in the site's graph, but semantically close.
     """
     name = models.CharField(max_length=50)
-    
+
     def to_dict(self):
         return {'id':self.pk, 'name':str(self.name)}
-    
+
