39031992aac439d5fd959c5efab69dbc98682264
C4ptainCrunch <nikita.marchant@gmail.com>
2013-03-02 16:45:37 +0100

    [fix] correct content-type for json responses (goes well with https://addons.mozilla.org/fr/firefox/addon/jsonview/)

--- graph/views.py
+++ graph/views.py
@@ -6,4 +6,4 @@ import json
 
 def node(req, nodeid):
     node = get_object_or_404(Node, pk=nodeid)
-    return HttpResponse(json.dumps(node.to_dict()))
+    return HttpResponse(json.dumps(node.to_dict()), content_type="application/json")
