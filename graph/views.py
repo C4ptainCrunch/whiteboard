101a0a0be015280d2d94ea1b7a5aff8ef57d2f6d
Titouan Christophe <titouanchristophe@gmail.com>
2013-03-03 11:00:35 +0100

    [enh] JSON API documentation

--- graph/views.py
+++ graph/views.py
@@ -5,10 +5,24 @@ from django.http import HttpResponse
 import json
 
 def node(req, nodeid):
+    """
+    GET /graph/<nodeid>
+    => {
+        'id' : int, 
+        'name' : str, 
+        'type' : str, 
+        'children' : list[{'id':int, 'name':str, 'type':str}, ...], 
+        ... 
+    }
+    """
     node = get_object_or_404(Node, pk=nodeid)
     return HttpResponse(json.dumps(node.to_dict()), content_type="application/json")
 
 
 def short(req, nodeid):
+    """
+    GET /graph/<nodeid>/short
+    => {'id':int, 'name':string, 'type':string}
+    """
     node = get_object_or_404(Node, pk=nodeid)
     return HttpResponse(json.dumps(node.short_dict()), content_type="application/json")
