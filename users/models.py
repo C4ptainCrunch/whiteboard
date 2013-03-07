ea91a8bcd33e99a19679fe55ab698b5be9d1a615
C4ptainCrunch <nikita.marchant@gmail.com>
2013-03-07 18:29:00 +0100

    [enh] User.to_dict() more verbose

--- users/models.py
+++ users/models.py
@@ -5,20 +5,26 @@ from django.contrib.auth.models import UserManager
 
 class User(AbstractBaseUser):
     netid = models.CharField(max_length=20,unique=True)
-#    last_name = models.CharField(max_length=100)
-#    first_name = models.CharField(max_length=100)
-#    email = models.EmailField()
     comment = models.CharField(null=True,max_length=1000)
     xml = models.CharField(max_length=10000)
     birth = models.DateField()
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    email = models.EmailField()
+
     USERNAME_FIELD = 'netid'
     REQUIRED_FIELDS = ('last_name','first_name','email')
     objects = UserManager()
 
     def to_dict(self):
         """Return self as a dict."""
-        res = {'netid':self.netid}
-        return res
+        return {
+            'netid':self.netid,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'full_name': self.first_name+' '+self.last_name,
+            'birth': str(self.birth),
+        }
 
 class UserIdentity(models.Model):
     matricule = models.CharField(max_length=50)
