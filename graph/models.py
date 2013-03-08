bad73964d0016973f3368818ca9dec0c98680969
C4ptainCrunch <nikita.marchant@gmail.com>
2013-03-08 16:03:36 +0100

    [add] node.detatch() [fix] node.childrens() not children()

--- graph/models.py
+++ graph/models.py
@@ -7,7 +7,7 @@ class Node(PolymorphicModel):
     """Base class for all P402 objects"""
     name = models.CharField(max_length=160)
     _children = models.ManyToManyField("self", symmetrical=False)
-    
+
     def classBasename(self):
         """Return the class name without modules prefix"""
         klass = str(type(self)) # "<class 'foo'>"
@@ -16,36 +16,36 @@ class Node(PolymorphicModel):
         while i>0 and klass[i-1]!='.':
             i -= 1
         return klass[i:j]
-    
-    
+
+
     def canonic_url(self):
         return '/'+self.classBasename().lower()+'/'+str(self.pk)
-    
-    
+
+
     def to_dict(self, with_children=False):
         res = {'id':self.pk, 'name':str(self.name), 'type':self.classBasename()}
         res['url'] = self.canonic_url()
         if with_children:
             res['children'] = []
-            for child in self.children():
+            for child in self.childrens():
                 res['children'].append(child.to_dict(False))
         return res
-    
-    
+
+
     def __repr__(self):
         return '<%s:%d "%s">'%(self.classBasename(), self.pk, self.name)
-    
-    
-    def children(self):
+
+
+    def childrens(self):
         """Return a list of all self's children"""
         return self._children.all()
-    
-    
+
+
     def ancestors(self):
         """Return a list of all self's ancestors"""
         return Node.objects.filter(_children=self)
-    
-    
+
+
     def hasCycle(self, traversed):
         """Recursively walk the graph to find any loop"""
         res = False
@@ -53,14 +53,14 @@ class Node(PolymorphicModel):
             res = True
         else:
             traversed.append(self)
-            for child in self.children():
+            for child in self.childrens():
                 if child.hasCycle(traversed):
                     res = True
                     break
             traversed.pop()
         return res
-    
-    
+
+
     def attach(self, child, acyclic_check=True):
         """
         Attach a new child to self and return True. If acyclic_check evaluates
@@ -74,20 +74,28 @@ class Node(PolymorphicModel):
             self._children.add(child)
             self.save()
         return res
-    
+
+    def detatch(self,parent):
+        """
+        Detatch self from parent. Return none
+        """
+        parent._children.remove(self)
+        parent.save()
+
+
+
 
 
 class Category(Node):
     description = models.TextField()
     lastmodif = models.DateTimeField(auto_now=True)
-    
+
     def save(self, *args, **kwargs):
         self.lastmodif = datetime.today()
         Node.save(self, *args, **kwargs)
-    
+
 
 
 class Taggable(Node):
     """An abstract taggable node. Taggable nodes have keywords."""
     keywords = models.ManyToManyField(Keyword)
-    
