a6806b91d0b6dcbcdb45dcd548c7f4e1ec27f661
iTitou <titouanchristophe@gmail.com>
2013-03-10 13:23:14 +0100

    [fix] Adapting edit url to adopted scheme

--- agora/urls.py
+++ agora/urls.py
@@ -3,5 +3,5 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
     (r'^(\d+)\.?(json|html)?$', 'agora.views.thread'),
-    (r'^edit/(\d+)$', 'agora.views.edit_thread'),
+    (r'^(\d+)/edit/?$', 'agora.views.edit_thread'),
 ) + staticfiles_urlpatterns()
